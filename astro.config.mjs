c786ece7c35a49e77221df7d59632a5751e3a31d
TDDY27 <jiataizang@gmail.com>
2026-03-10 21:28:34 +0800

    [BugFix] Fix the astro.config.mjs to solve change domain problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
-  base: '/NTUAIS/',
-  site: 'https://ntuais.github.io',
+  // base: '/NTUAIS/',
+  // site: 'https://ntuais.github.io',
+  site: 'https://ntuais.org',
   output: 'static',
   markdown: {
     remarkPlugins: [remarkMath],
